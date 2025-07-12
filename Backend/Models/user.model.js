3728a77fd486c9bcf3e82c6cf67a0b4a32fa0467
Aditya rai <raijiaditya9868@gmail.com>
2025-07-12 22:12:17 +0530

    created models

---
+++ Backend/Models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, // optional: prevent duplicate emails
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
